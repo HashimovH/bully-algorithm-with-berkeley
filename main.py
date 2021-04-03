9a7e878af3086dd8721a6ff63be8d3a794d2346d
aleks96n <aleksnik1996@gmail.com>
2021-04-03 16:04:02 +0300

    automatic, reload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from create_process import create
 from node import Node
 import socket
 import time
+import threading
 
 def send_message(message, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
@@ -22,16 +23,15 @@ def send_message(message, port):
 class Network:
 	current_port = randint(10000, 60000)
 	node_list = []
+	suspended_list = []
+	coordinator = None
 	def __init__(self, nodes):
-		biggest_node = None
 		for node in nodes:
 			# add each node to the network with Node class
 			# by default, change K to 0
 			node_label = node[1].split("_")[0]
 			# Create Node obj to store in the node_list
 			node_obj = Node(node[0], node_label+"_0", node[2][:-2], self.current_port)
-			if(biggest_node == None or biggest_node.id_ < node_obj.id_):
-				biggest_node = node_obj
 			create(node_obj.id_, node_obj.label, node_obj.time, node_obj.port)
 			#is this needed?
 			#time.sleep(1)
@@ -42,8 +42,9 @@ class Network:
 				print(f"Node {node_obj.label} has been created on PORT {self.current_port}")
 			
 			self.current_port += 1
-		self.default_election(biggest_node)
-		self.sync_clocks(biggest_node, self.node_list)
+		self.coordinator = self.startElection(default = True)
+		send_message("make coordinator", self.coordinator.port)
+		self.sync_clocks()
 		time.sleep(1)
 
 
@@ -53,19 +54,13 @@ class Network:
 		if command.strip() == ('list'):
 			# Let's check our each node to know it is there or not
 			for node in self.node_list:
-				# if send_message("here?", node.port) == "yes":
-				is_coordinator = send_message("is_coordinator", node.port)
-				#if is_coordinator == "yes":
-					#message += f"{node.id_}, {node.label} (Coordinator)\n"
-				#else:
-					#message += f"{node.id_}, {node.label}\n" # This should be changed. We should collect node objects not tuples.
-				message += node.identify() + '\n'
+				message += send_message("list", node.port) + '\n'
+
 		elif command.strip() == "clock":
 			for node in self.node_list:
-				#message += f"{node.label}, {node.time}\n" # This should be changed. We should collect node objects not tuples.
-				message += node.clockTime() + '\n'
+				message += send_message("clock", node.port) + '\n'
 
-		elif command.startswith('kill'): # Has problem
+		elif command.startswith('kill'): #Solved?
 			id_ = command.split(' ')[1]
 			for node in self.node_list:
 				if node.id_ == id_:
@@ -75,20 +70,52 @@ class Network:
 					if node.is_coordinator:
 						self.startElection()
 					break
-		elif command.startswith('set-time'):
-			id_ = command.split(' ')[1]
-			time = command.split(' ')[2]
-			self.setTime(id_, time)
+
+		elif command.startswith('freeze'):
+			id_ = int(command.split(' ')[1])
+			print(f"Freezing id {id_}")
+			for i in self.node_list:
+				if int(i.id_)==id_:
+					reply = send_message("freeze", i.port)
+					if  reply == "freezed":
+						print(f"Freezing Node with ID {i.id_} on PORT:{i.port}")
+						self.suspended_list.append(i)
+						self.node_list.remove(i)
+					if i.is_coordinator:
+						self.startElection()
+						self.sync_clocks()
+					break
+
+		elif command.startswith('unfreeze'):
+			id_ = int(command.split(' ')[1])
+			for i in self.suspended_list:
+				if int(i.id_)==id_:
+					reply = send_message("unfreeze", i.port)
+					if  reply == "unfreezed":
+						print(f"Unfreezing Node with ID {i.id_} on PORT:{i.port}")
+						self.suspended_list.remove(i)
+						self.node_list.append(i)
+					self.startElection()
+					self.sync_clocks()
+					break
+
+		elif command.startswith("reload"):
+			file = command.split(' ')[1]
+			self.reload(file)
 
 		print(message)
+	#drift
+	def testOmega(self):
+		while True:
+			time.sleep(60)
+			send_message("update", self.coordinator.port)
+			self.coordinator.updateTime()
+			self.sync_clocks()
 
-	#is this java?
-	def default_election(self, node):
-		node.make_coordinator()
 
-	def startElection(self):
+	def startElection(self, default = False):
 		for node in self.node_list:
-			node.resetTime()
+			send_message("reset", node.port)
 		#didn't specify who starts it
 		randomProcess = choice(self.node_list)
 		#sorts by id_ (yes it does sort, i hope)
@@ -98,36 +125,80 @@ class Network:
 		while (ind != len(sortedList)):
 			randomProcess = sortedList[ind]
 			for node in sortedList[ind:]:
-				if(node.processMessages("here?") != "yes"):
+				if(send_message("here?", node.port) != "yes"):
 					sortedList.pop(node)
 			ind+=1
 
 		randomProcess.make_coordinator()
+		self.coordinator = randomProcess
+		send_message("make coordinator", self.coordinator.port)
 		print(f"Process {randomProcess.id_} is the new coordinator")
-		self.sync_clocks(randomProcess, self.node_list, True)
+		if(default):
+			for node in self.node_list:
+				node.time = randomProcess.time
+		else:
+			self.sync_clocks(True)
+		return randomProcess
 
-
-	#hmmm
-	def apply_bully(self):
-		pass
 
 	def setTime(self, process_id, time):
 		for i in range(len(self.node_list)):
 			if(self.node_list[i].id_ == process_id):
 				self.node_list[i].updateTime(time)
 				if(self.node_list[i].is_coordinator):
-					self.sync_clocks(self.node_list[i], self.node_list, True)
+					self.sync_clocks(True)
 				#if the process was not the coordinator, sync must happen automatically
 				break
 
 
-	def sync_clocks(self, coordinator, nodes, newProcess = False):
-		s = coordinator.time
-		for node in nodes:
-			node.time = s
+	def sync_clocks(self, newProcess = False):
+		s = send_message("time", self.coordinator.port)
+		for node in self.node_list:
+			send_message(f"set {s}", node.port)
+			#node.time = s
 			if(newProcess):
-				node.K+=1
-				node.update_label()
+				send_message("label", node.port)
+				#node.update_label()
+
+	def reload(self, filename):
+		file_content = open(filename, "r")
+		helper = {}
+		for line in file_content:
+			helper[int(line.split(",")[0])] = [line.split(",")[1], line.split(",")[2].strip()]
+		for i in self.node_list:
+			print(i.id_)
+			if(int(i.id_ )in list(helper.keys())):
+				helper.pop(int(i.id_))
+
+		print(helper)
+
+		for s in self.suspended_list:
+			if(s.id_ in list(helper.keys())):
+				helper.pop(int(s.id_))
+
+		for h in helper:
+			label = helper[h][0]
+			time_l = helper[h][1]
+			node_label = label.split("_")[0]
+			# Create Node obj to store in the node_list
+			node_obj = Node(h, node_label+"_0", time_l[:-2], self.current_port)
+			create(node_obj.id_, node_obj.label, node_obj.time, node_obj.port)
+			reply = send_message("here?", node_obj.port)
+			if reply == "yes":
+				# self.node_list.append((node[0], node_label+"_0", node[2], self.current_port))
+				self.node_list.append(node_obj)
+				print(f"Node {node_obj.label} has been created on PORT {self.current_port}")
+			
+			self.current_port += 1
+
+		self.coordinator = self.startElection(default = True)
+		send_message("make coordinator", self.coordinator.port)
+		self.sync_clocks()
+		time.sleep(1)
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -148,10 +219,9 @@ if __name__ == '__main__':
 	# Create the network
 	net = Network(nodes)
 	print("Network has been created")
-
 	# Select coordinator
-
 	# get commands and process
+	threading.Thread(target=lambda: net.testOmega()).start()
 	while True:
 		print('Enter your command: ', end='$ ')
 		cmd = input()
