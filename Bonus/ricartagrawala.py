f6f06259168b3bd089850f6b126170cd363534f3
aleks96n <aleksnik1996@gmail.com>
2021-04-06 13:35:35 +0300

    added comments

--- Bonus/ricartagrawala.py
+++ Bonus/ricartagrawala.py
@@ -19,7 +19,8 @@ def send_message(message, port):
         sc.send(message.encode('utf-8'))
         r_msg = sc.recv(2048).decode('utf-8')
         return r_msg
-
+#change this to a class maybe? I dunno
+#but this is the critical section, it doesn't do anything other than signify when the thread is done
 def CS(process_id, node_):
 	print(f"P{process_id} is done with critical section")
 	send_message("done", node_.port)
@@ -27,22 +28,30 @@ def CS(process_id, node_):
 
 class Network:
 	current_port = randint(10000, 60000)
+	#all the nodes
 	node_list = []
+	#the queue of nodes wanting CS
 	queue_list = []
+	#hashmap showing which node belongs to what thread
 	threadToNode = {}
-	#don't like this
+	#don't like this, but this shows if there is a proccess running
 	running = False
+
+	#create port and timestamp, simple
 	def __init__(self, nodes):
 		for node in nodes:
 			process = Node(node[1], self.current_port)
 			self.current_port += 1
 			self.node_list.append(process)
-	#change this to a class maybe? I dunno
+	
 
-
+	#queue priority
+	#create a thread for 10 seconds (should be 30, but testing)
+	#and we have to see when we should run the thread
 	def prioritize(self, node):
 		head = send_message("id", node.port)
 		t = threading.Timer(10, CS, args = (head, node,))
+		#if nothing is in the queue, put it at the start and run, simple
 		if(len(self.queue_list) == 0):
 			#don't like this either
 			self.queue_list.append(node)
@@ -54,24 +63,35 @@ class Network:
 			id_ = len(self.queue_list) - 1
 			for node_ in self.node_list:
 				response = send_message("access", node_.port)
+				#empty response means that it is using CS or timeStamp is lower, meaning higher priority
 				if(response == ""):
 					continue
+				#this means that the timeStamp is higher, so the node has priority
 				elif(response == "OK"):
-					if(node_ in queue_list):
-						id_ = min(id_, self.queue_list.index(node_))
+					#track the best position for our node
+					id_ = min(id_, self.queue_list.index(node_))
+			#add it in the queue
 			self.queue_list.insert(id_)
+			#mark the thread
 			self.threadToNode[node] = t
+			#send the message that it is wanted
 			send_message("wanted", node.port)
-
+	#this is a constant function that runs ALWAYS, so be careful
 	def cycle(self):
+		#to make sure the thread always runs
 		while(True):
+			#to make sure this part only runs when there is something in the queue
 			while(self.running):
+				#test
 				print("got here")
+				#if only one element in the queue, that means it's the last one
+				# and we can stop this inner while cycle and reset the lists
 				if(len(self.queue_list) == 1):
 					self.running = False
 					self.queue_list = []
 					self.threadToNode = {}
-
+				#check to see if the first process in the queue list is running
+				#if it is not, time to pop it and run the second one
 				elif(self.threadToNode[self.queue_list[0]].is_alive() == False):
 					toRemove = queue_list.pop(0)
 					self.threadToNode.pop(toRemove)
